e656fc489f123824978a560ec1dd4056df1fc6ff
mdewinged <alexandreamk1@gmail.com>
2022-01-26 12:34:20 -0300

    .
    .

--- main.cpp
+++ main.cpp
@@ -6,20 +6,26 @@ Author:
 #include <iostream>
 #include <string.h>
 
+
 using namespace std;
+
 
 #define OPTION_PRE "-p"
 #define OPTION_OBJ "-o"
 
 
-void arguments(int argc, char const *argv[]){
-    cout << OPTION_PRE << '\n';
-    cout << OPTION_OBJ << '\n';
-}
-
-
 int main(int argc, char const *argv[])
 {
-    arguments(argc, argv);
+    if(strcmp(argv[1], OPTION_PRE)==0){
+        cout << "option -p chosen\n"
+    }
+    else if(strcmp(argv[1], OPTION_OBJ)==0){
+        cout << "option -o chosen\n"
+    }
+    else{
+        cout << "UnB - Software Básico - Assembler\n\nCommands:\n.\
+        /montador -p [programname].asm\n./montador -p [programname]\
+        .asm\n\nInvalid option. Use one of the optionss above.\n"
+    }
     return 0;
 }
