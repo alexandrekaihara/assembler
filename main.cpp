2684e81cc5eff1d424ba58bba7c5812580e8e258
mdewinged <alexandreamk1@gmail.com>
2022-01-26 12:05:12 -0300

    .

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+Brief:
+Author: 
+*/
+
+#include <iostream>
+
+
+using namespace std;
+
+
+void arguments(int argc, char const *argv[]){
+    for (int i=0; i<argc; i++) cout << argv[i];    
+}
+
+
+int main(int argc, char const *argv[])
+{
+    arguments(argc, argv);
+    return 0;
+}
