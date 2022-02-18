ec7e63d4c1a22fd7c824929f0404c801666eec8d
mdewinged <alexandreamk1@gmail.com>
2022-02-18 11:46:56 -0300

    .

--- main.cpp
+++ main.cpp
@@ -30,32 +30,23 @@ int main(int argc, char const *argv[])
     int option=0;
     char* filename;
 
-    // Interprets the program input
+    // If the program receives any different number of parameters than 4, ends program
     if(argc != 4){
         cout << INSTRUCTIONS;
         exit(0);
     }
-    if(strcmp(argv[1], OPTION_PRE)==0 && argc == 4){
-        cout << "option -p chosen\n";
-        option = OPTION_PRE_NUM;
-        filename = (char*)argv[2];
-    }
-    else if(strcmp(argv[1], OPTION_OBJ)==0 && argc == 4){
-        cout << "option -o chosen\n";
-        option = OPTION_OBJ_NUM;
-        filename = (char*)argv[2];
-    }
-    else if(strcmp(argv[1], OPTION_MAC)==0 && argc == 4){
-        cout << "option -o chosen\n";
-        option = OPTION_MAC_NUM;
-        filename = (char*)argv[2];
-    }
+
+    // Set interpret the chosen option
+    if(strcmp(argv[1], OPTION_PRE)==0)      option = OPTION_PRE_NUM;
+    else if(strcmp(argv[1], OPTION_OBJ)==0) option = OPTION_OBJ_NUM;
+    else if(strcmp(argv[1], OPTION_MAC)==0) option = OPTION_MAC_NUM;
     else{
         cout << INSTRUCTIONS;
         cout << "\nInvalid option. Use one of the options above.\n";
         exit(0);
     }
-
+    
+    filename = (char*)argv[2];
     Assembler* assembler = new Assembler(option, filename);
     //assembler->run();
 
