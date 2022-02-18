5d613e560bf18876be6efeb59260d2c717c1c5ac
mdewinged <alexandreamk1@gmail.com>
2022-02-18 15:16:03 -0300

    .

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -11,11 +11,11 @@ using namespace std;
 LexicalAnalyzer::LexicalAnalyzer(unordered_map<string, int, int> SymbolsTable,\
             unordered_map<string, int, int> DirectivesTable,\
             unordered_map<string, int, int, int> InstructionsTable){
-    this.SymbolsTable = SymbolsTable;
-    this.DirectivesTable = DirectivesTable;
-    this.InstructionsTable = InstructionsTable;
+    this->SymbolsTable = SymbolsTable;
+    this->DirectivesTable = DirectivesTable;
+    this->InstructionsTable = InstructionsTable;
 
-    cout << this.SymbolsTable["ZERO"];
-    cout << this.DirectivesTable["SPACE"];
-    cout << this.InstructionsTable["ADD"];
+    cout << this->SymbolsTable["ZERO"];
+    cout << this->DirectivesTable["SPACE"];
+    cout << this->InstructionsTable["ADD"];
 }
