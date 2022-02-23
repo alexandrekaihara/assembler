2b4690c5298927d8534c5363c4ae01631719c01e
mdewinged <alexandreamk1@gmail.com>
2022-02-22 22:56:36 -0300

    .

--- test.cpp
+++ test.cpp
@@ -27,8 +27,8 @@ void TestLex::test(){
     
     // Test if it cleans the first whitespaces
     res = this->Lex->clean_line("  COPY A, B");
-    string aux = "COPY A, B";
-    assert(res.compare(aux) == 0);
+    cout << res << " " << to_string(res.compare("COPY A, B")) << "\n";
+    assert(res.compare(" COPY A, B") == 0);
 
     // Test if it removes the double spaces
     res = this->Lex->clean_line("  COPY  A,      B");
