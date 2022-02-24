775a3f38bc7dce860a7c3a121db8be31117efeeb
mdewinged <alexandreamk1@gmail.com>
2022-02-24 12:19:58 -0300

    .

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -40,7 +40,7 @@ string LexicalAnalyzer::clean_line(string line){
     
     // Remove all char on pattern
     string pattern = "\t\n", newlinestr;
-    for(; i<=j; i++){
+    for(; i<j; i++){
         if(pattern.find(line[i]) != -1) continue;
         if ((line[i] == ' ') && (line[i+1] == ' ')) continue;
         newlinestr += line[i];
