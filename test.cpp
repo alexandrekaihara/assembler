0e5553bfd8dd32e3cae575adee3e5246ddd80a56
mdewinged <alexandreamk1@gmail.com>
2022-02-25 17:28:30 -0300

    .

--- test.cpp
+++ test.cpp
@@ -24,7 +24,7 @@ bool assembly_file(char* file, vector<int> correct){
     Assembler* A = new Assembler(OPTION_MAC_NUM, input, output);
     A->run();
     cout << A->Err->get_errors().size() << "\n";
-    for(int i=0; i<A->Err->get_errors().size(); i++) cout << A->Err->get_errors()[i] << " ";
+    for(int i=0; i<A->Err->get_errors().size(); i++) cout << A->Err->get_errors()[i].code << " ";
     cout << "\n";
     return check_vectors(A->Err->get_errors(), correct);
 }
